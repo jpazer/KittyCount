b4c1968404e58d30116df361879813c533b9df46
Jasmine Pazer <jgp5980@rit.edu>
2018-05-02 08:25:52 -0400

    make error text dynamic

--- KittyCount.activity/source/Utilities.py
+++ KittyCount.activity/source/Utilities.py
@@ -22,10 +22,15 @@ class Utilities:
 
     @staticmethod
     def show_error(screen, error_text):
-        # shows helpful errors
-
+        # get ui y
         w, h = Utilities.get_width_height()
-        x, y = (w/2, h/2 + 120)
+        ui_height = 160
+        y = h/2
+        spacing = (h - y - ui_height) / 2
+        y = y + spacing * (3/4)
+
+        # shows helpful errors
+        x = w/2
         pygame.draw.rect(screen, (255, 255, 255), (x-250, y, 500, 20), 0)
         text_surface = Utilities.make_text_surface(error_text, 16)
         text_size = text_surface.get_size()
